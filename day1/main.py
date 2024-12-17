de3a5900beaa167d8b733856764a9cba05c2a380
Mohamed Elanaami <97893781+MedElanaami@users.noreply.github.com>
2024-12-17 12:40:40 +0100

    day 1

---
+++ day1/main.py
@@ -0,0 +1,21 @@
+left_numbers=[]
+right_numbers=[]
+with open("input.txt") as file:
+    for line in file:
+         # Split the line into two numbers
+        numbers = line.split()
+        if len(numbers) == 2:
+            left_numbers.append(int(numbers[0]))
+            right_numbers.append(int(numbers[1]))
+
+#sort the arrays
+left_numbers.sort()
+right_numbers.sort()
+
+# calculate the difference between the arrays in same index
+diff = [abs(left_numbers[i] - right_numbers[i]) for i in range(len(left_numbers))]
+
+#sum of diff array
+sum_diff = sum(diff)
+
+print(sum_diff)
